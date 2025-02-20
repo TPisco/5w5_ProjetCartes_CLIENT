e9a5520a66b4b7aad2dc9374ff7e4958ae97b66f
thanh <2355336@cegepmontpetit.ca>
2025-02-20 13:57:36 -0500

    FCT: Début interface OwnedCards
    On a commencé l'interface HTML de mes cartes. Il manque la liste des cartes.

--- ngsci-depart-main/src/app/app-routing.module.ts
+++ ngsci-depart-main/src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MatchComponent } from './match/match.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { OwnedcardsComponent } from './components/ownedcards/ownedcards.component';
 
 const routes: Routes = [
   { path: 'match/:id', component: MatchComponent },
-  { path: '', component: HomeComponent, children: [
-    { path: '', component: WelcomeComponent },
-  ]},
-  { path: '**', redirectTo: '/'}
+  {
+    path: '', component: HomeComponent, children: [
+      { path: '', component: WelcomeComponent },
+
+      { path: 'OwnedCards', component: OwnedcardsComponent }
+    ]
+  },
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
---
+++ ngsci-depart-main/src/app/components/ownedcards/ownedcards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ownedcards',
+  templateUrl: './ownedcards.component.html',
+  styleUrls: ['./ownedcards.component.css']
+})
+export class OwnedcardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
