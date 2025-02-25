fb42ca3cd666f9b1c59baf93bdd8d888e8fc7c65
RLapre <6223627@cegepmontpetit.ca>
2025-02-25 11:53:55 -0500

    Bugfix : Correction de la mise en page html et css de MagasinComponent et ajout de la liste des cartes dans OwnedCardsComponent

--- ngsci-depart-main/src/app/components/ownedcards/ownedcards.component.ts
+++ ngsci-depart-main/src/app/components/ownedcards/ownedcards.component.ts
@@ -1,25 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Card } from 'src/app/models/models';
 import { ApiService } from 'src/app/services/api.service';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-ownedcards',
   templateUrl: './ownedcards.component.html',
   styleUrls: ['./ownedcards.component.css'],
-  standalone:true,
-  imports: [RouterModule, RouterOutlet]
+  standalone: true,
+  imports: [RouterModule, RouterOutlet, FormsModule, CommonModule, MatCardModule, CardComponent]
 
 })
 export class OwnedcardsComponent implements OnInit {
 
-  public listOwnedCards : Card[] = [];
-  constructor(public service : ApiService) { }
+  public listOwnedCards: Card[] = [];
+  constructor(public service: ApiService) { }
 
- async ngOnInit() {
+  async ngOnInit() {
     this.listOwnedCards = await this.service.getPlayersCards();
-    if(this.listOwnedCards == null){
-     console.log("La liste ne contient aucune carte.")
+    if (this.listOwnedCards == null) {
+      console.log("La liste ne contient aucune carte.")
     }
   }
 
