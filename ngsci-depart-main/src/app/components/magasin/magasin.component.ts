d2a095c657dc89d38e3ab4ffe0e6f738a06c25b5
RLapre <6223627@cegepmontpetit.ca>
2025-02-25 10:56:01 -0500

    FCT: Ajout de l'affichage de la liste des cartes dans MagasinComponent

--- ngsci-depart-main/src/app/components/magasin/magasin.component.ts
+++ ngsci-depart-main/src/app/components/magasin/magasin.component.ts
@@ -1,15 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Card } from 'src/app/models/models';
+import { ApiService } from 'src/app/services/api.service';
+import { MatCardModule } from '@angular/material/card';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-magasin',
   templateUrl: './magasin.component.html',
-  styleUrls: ['./magasin.component.css']
+  styleUrls: ['./magasin.component.css'],
+  standalone: true,
+  imports: [RouterModule, RouterOutlet, FormsModule, CommonModule, MatCardModule, CardComponent]
 })
 export class MagasinComponent implements OnInit {
 
-  constructor() { }
+  public listCards: Card[] = [];
 
-  ngOnInit() {
+  constructor(public service: ApiService) { }
+
+  async ngOnInit() {
+    this.listCards = await this.service.getAllCards();
+
+    if (this.listCards == null) {
+      console.log("La liste ne contient aucune carte.")
+    }
+
   }
+
+
 
 }
--- ngsci-depart-main/src/app/components/ownedcards/ownedcards.component.ts
+++ ngsci-depart-main/src/app/components/ownedcards/ownedcards.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Card } from 'src/app/models/models';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-ownedcards',
   templateUrl: './ownedcards.component.html',
-  styleUrls: ['./ownedcards.component.css']
+  styleUrls: ['./ownedcards.component.css'],
+  standalone:true,
+  imports: [RouterModule, RouterOutlet]
+
 })
 export class OwnedcardsComponent implements OnInit {
 
-  constructor() { }
+  public listOwnedCards : Card[] = [];
+  constructor(public service : ApiService) { }
 
-  ngOnInit() {
+ async ngOnInit() {
+    this.listOwnedCards = await this.service.getPlayersCards();
+    if(this.listOwnedCards == null){
+     console.log("La liste ne contient aucune carte.")
+    }
   }
 
 }
