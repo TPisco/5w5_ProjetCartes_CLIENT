2eb3030e70c9f90fcd1f0d3c23617c431a58c57b
thanh <2355336@cegepmontpetit.ca>
2025-03-06 12:36:00 -0500

    Revert "Merge branch 'Authentification'"
    This reverts commit 98c941be6ae6af96d2df4c18665994586aa0144b, reversing
    changes made to 469d76bb5f01af94a42aac9017333c7c24116f1f.

--- ngsci-depart-main/src/app/app.component.ts
+++ ngsci-depart-main/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatButtonModule } from '@angular/material/button';
-import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -24,36 +23,18 @@ import { ApiService } from './services/api.service';
 export class AppComponent {
   title = 'supercartesinfinies';
 
-  constructor(public router: Router, public matchService: MatchService, public ApiServices : ApiService) {
-    if (!this.isLogged()){
-      this.router.navigate(["/login"])
-    }
-  }
+  constructor(public router: Router, public matchService: MatchService) {}
 
   isLogged() {
     // TODO: Gérer l'affichage du joueur lorsqu'il est connecté
-   if (sessionStorage.getItem("token") != null)
-   {
-    return true
-   }
-      return false;
+    return true;
   }
 
   getUsername() {
-    if (sessionStorage.getItem("username") != null) {
-      return sessionStorage.getItem("username")
-    }
     return 'USERNAME';
   }
 
   async logout() {
-    sessionStorage.removeItem("token");
-    sessionStorage.removeItem("playerId");
-    sessionStorage.removeItem("username");
-    this.router.navigate(["/login"])
-  }
-
-  async Test(): Promise<void> {
-    this.ApiServices.test();
+    // TODO: Gérer le logout
   }
 }
