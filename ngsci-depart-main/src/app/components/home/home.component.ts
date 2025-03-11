a00bd4ecbd6b3dfa46572b5a8472e41c0ea44c32
Pisco <6237456@cegepmontpetit.ca>
2025-03-11 11:38:17 -0400

    FCT: Ajout du code nécéssaire pour connection et Join Match
    le code nécessaire pour Join match et pour la connexion a été débuté, mais ne fonctionne pas

--- ngsci-depart-main/src/app/components/home/home.component.ts
+++ ngsci-depart-main/src/app/components/home/home.component.ts
@@ -4,6 +4,7 @@ import { MatchService } from '../../services/match.service';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import * as signalR from "@microsoft/signalr"
 
 @Component({
   selector: 'app-home',
@@ -15,11 +16,13 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 export class HomeComponent implements OnInit {
 
   isSearchingMatch: boolean = false;
+  isConnected:Boolean = false;
   searchMessage: string = '';
+  private hubConnection?: signalR.HubConnection
   constructor(public router: Router, public match: MatchService) { }
 
   ngOnInit() {
-
+    this.setupSignalRConnection();
   }
 
 
@@ -32,21 +35,69 @@ export class HomeComponent implements OnInit {
   }
 
 
+  private setupSignalRConnection() {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('http://localhost:7179/match', {
+        accessTokenFactory: () => sessionStorage.getItem("token")! // Provide the token
+      })
+      .build();
+
+    // Listen for "JoiningMatch" event
+    this.hubConnection.on('JoiningMatch', (joiningMatchData: any) => {
+      this.isSearchingMatch = false; // Stop searching once match is found
+      this.searchMessage = 'Match trouvé!';
+      let matchId = joiningMatchData.match.id; // Get the match ID
+      this.router.navigate(['/match/' + matchId]); // Navigate to match page
+    });
+
+    // Start connection
+    this.hubConnection.start()
+      .then(() => {
+        console.log('SignalR connection established');
+      })
+      .catch(err => {
+        console.error('Error while starting connection: ', err);
+      });
+  }
+
+
+
+  
   joinMatch() {
+    if (this.hubConnection?.state !== signalR.HubConnectionState.Connected) {
+
+      this.hubConnection?.start()
+        .then(() => {
+          console.log('SignalR connection established');
+          this.sendJoinMatchRequest();
+        })
+        .catch(err => {
+          console.error('Error while starting connection: ', err);
+        });
+    } else {
+
+      this.sendJoinMatchRequest();
+    }
+  }
 
 
+
+  // Send the join match request to the SignalR Hub
+  private sendJoinMatchRequest() {
     this.isSearchingMatch = true;
     this.searchMessage = 'Recherche d\'un match...';
-    // TODO: Anuglar: Afficher un dialogue qui montre que l'on attend de joindre un match
-    // TODO: Hub: Se connecter au Hub et joindre un match
 
+    // Call the server method to join a match (send the user info, etc.)
+    this.hubConnection?.invoke('onJoinMatchAsync', sessionStorage.getItem("userId"), this.hubConnection!.connectionId, null)
+      .catch(err => console.error('Error while sending join match request: ' + err));
+
+    // Optionally handle a timeout scenario if no match is found within a reasonable time
     setTimeout(() => {
-      this.searchMessage = 'Match trouvé!';
-      let matchId = -1;
-      this.router.navigate(['/match/' + matchId]);
-      this.isSearchingMatch = false;
-    }, 5000);
+      if (this.isSearchingMatch) {
+        this.searchMessage = 'Aucun match trouvé. Essayez à nouveau.';
+        this.isSearchingMatch = false;
+      }
+    }, 5000); // 5-second timeout for demonstration
   }
 }
-
 
--- ngsci-depart-main/src/app/services/api.service.ts
+++ ngsci-depart-main/src/app/services/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Card } from '../models/models';
@@ -8,8 +8,8 @@ import { Card } from '../models/models';
 })
 export class ApiService {
 
-  //serverUrl = "https://localhost:7179/";
-  serverUrl = "http://localhost:5276/";
+  serverUrl = "https://localhost:7179/";
+  //serverUrl = "http://localhost:5276/";
 
 
   constructor(public http: HttpClient) { }
@@ -20,7 +20,56 @@ export class ApiService {
   }
 
   async getPlayersCards(): Promise<Card[]> {
-    let result = await lastValueFrom(this.http.get<Card[]>(this.serverUrl+'api/card/GetPlayersCards'));
+    let token = sessionStorage.getItem("token");
+    let httpOptions = {
+      headers : new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Authorization' : 'Bearer ' + token
+      })
+    };
+    let result = await lastValueFrom(this.http.get<Card[]>(this.serverUrl+'api/card/GetPlayersCards',httpOptions));
     return result;
   }
+
+  async register(email: string, password: string, passwordConfirm: string): Promise<void> {
+    let registerDTO = {
+      Email: email,
+      Password: password,
+      PasswordConfirm: passwordConfirm
+    };
+ 
+    let x = await lastValueFrom(this.http.post<any>(this.serverUrl + "api/Players/Register", registerDTO))
+    console.log(x);
+  }
+ 
+  async login(username: string, password: string): Promise<void> {
+ 
+    let loginDTO = {
+      Username: username,
+      Password: password
+    };
+ 
+    let x = await lastValueFrom(this.http.post<any>(this.serverUrl + "api/Players/Login", loginDTO));
+    console.log(x);
+ 
+    sessionStorage.setItem("token", x.token);
+    sessionStorage.setItem("playerId", x.playerId);
+    sessionStorage.setItem("username", x.username);
+ 
+  }
+ 
+  async test(): Promise<any> {
+    let token = sessionStorage.getItem("token");
+    let httpOptions = {
+      headers : new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Authorization' : 'Bearer ' + token
+      })
+    };
+ 
+    let x = await lastValueFrom(this.http.get<any>(this.serverUrl + "api/Players/PrivateData", httpOptions))
+    console.log(x);
+    return x
+  }
+ 
 }
