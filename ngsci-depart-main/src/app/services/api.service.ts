7dacd30568dc27e10b0bcbc6f929f8fde7473817
RLapre <6223627@cegepmontpetit.ca>
2025-04-17 15:01:10 -0400

    Progress

--- ngsci-depart-main/src/app/services/api.service.ts
+++ ngsci-depart-main/src/app/services/api.service.ts
@@ -18,30 +18,34 @@ export class ApiService {
   constructor(public http: HttpClient) { }
 
   async getAllCards(): Promise<Card[]> {
-    let result = await lastValueFrom(this.http.get<Card[]>(this.serverUrl+'api/card/GetAllCards'));
+    let result = await lastValueFrom(this.http.get<Card[]>(this.serverUrl + 'api/card/GetAllCards'));
     return result;
   }
 
   async getPlayersCards(): Promise<Card[]> {
     let token = sessionStorage.getItem("token")
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    let result = await lastValueFrom(this.http.get<Card[]>(this.serverUrl+'api/card/GetPlayersCards', {headers}));
+    let result = await lastValueFrom(this.http.get<Card[]>(this.serverUrl + 'api/card/GetPlayersCards', { headers }));
     return result;
   }
 
-async getPlayerDecks(): Promise<Deck[]>{
-    let result = await lastValueFrom(this.http.get<Deck[]>(this.serverUrl + 'api/deck/GetPlayerDecks'))
-  return result;
-}
+  async getPlayerDecks(): Promise<Deck[]> {
+    //Le token est nécessaire pour la requête, sinon erreur "Unauthorized"
+    let token = sessionStorage.getItem("token")
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    let result = await lastValueFrom(this.http.get<Deck[]>(this.serverUrl + 'api/Deck/GetPlayerDecks', { headers }))
+    return result;
+  }
 
 
-async CreateDeck(nom : string): Promise<Deck[]>{
+  async CreateDeck(nom: string): Promise<Deck[]> {
     //let deckDTO = {
     //   Name : nom
     //};
+
     let result = await lastValueFrom(this.http.post<Deck[]>(this.serverUrl + 'api/deck/CreateDeck', nom))
-  return result;
-}
+    return result;
+  }
 
 
 
@@ -75,9 +79,9 @@ async CreateDeck(nom : string): Promise<Deck[]>{
   async test(): Promise<any> {
     let token = sessionStorage.getItem("token");
     let httpOptions = {
-      headers : new HttpHeaders({
-        'Content-Type' : 'application/json',
-        'Authorization' : 'Bearer ' + token
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
       })
     };
 
