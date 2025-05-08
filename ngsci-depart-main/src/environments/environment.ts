579c18f315488afbf9fe064d7374654b8551dc1f
Pisco <6237456@cegepmontpetit.ca>
2025-05-08 14:57:43 -0400

    FCT: Changement de Route

--- ngsci-depart-main/src/environments/environment.ts
+++ ngsci-depart-main/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
      production: true,
-    apiUrl: "https://apisupercartesinfinies.azurewebsites.net/"
+    apiUrl: "https://serveurcartepokemon-erayh3cseybtasdw.canadacentral-01.azurewebsites.net/"
 };
